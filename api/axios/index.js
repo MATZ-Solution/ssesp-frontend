5384ef3156c0d1b5e2f2120f6a92385d4c74bf83
sohaibnayyar721 <sohaibnayyar.721@gmail.com>
2026-02-14 18:30:52 +0500

    changes in project 2

--- api/axios/index.js
+++ api/axios/index.js
@@ -10,7 +10,7 @@ const api = axios.create({
   headers: {
     "Content-Type": "application/json",
   },
-  timeout: 10000, // optional timeout
+  timeout: 10000, // optional timeouts
 });
 
 // Optional: Add interceptors for auth, logging, errors
